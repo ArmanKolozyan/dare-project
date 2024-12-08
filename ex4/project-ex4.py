35b5ad969faec7f1460d04c5ff8f75aa4f7ef60e
JolienAndries <94374597+JolienAndries@users.noreply.github.com>
2024-12-08 22:20:36 +0100

    added exercise 2
    (and some small changes in comments)

--- ex4/project-ex4.py
+++ ex4/project-ex4.py
@@ -44,6 +44,11 @@ def remove_op(signing_key, removed_key, preds):
     ``preds`` is a list of hashes of immediate predecessor operations."""
     return sign_msg(signing_key, {'type': 'remove', 'removed_key': removed_key, 'preds': preds})
 
+def post_op(signing_key, message, preds):
+    """Returns an operation signed by ``signing_key``, which adds a ``message`` to the group.
+    ``preds`` is a list of hashes of immediate predecessor operations."""
+    return sign_msg(signing_key, {'type': 'post', 'message': message, 'preds': preds})
+
 def interpret_ops(ops):
     """
     Takes a set of access control operations and computes the currently authorised set of users.
@@ -53,13 +58,19 @@ def interpret_ops(ops):
     ops_by_hash = {hex_hash(op): verify_msg(op) for op in ops}
     parsed_ops = ops_by_hash.values()
 
-    # Every op must be one of the expected types
-    if any(op['type'] not in {'create', 'add', 'remove'} for op in parsed_ops):
-        raise Exception('Every op must be either create, add, or remove')
+    # Every op must be one of the expected types 
+    # (added post for exercise 2)
+    if any(op['type'] not in {'create', 'add', 'remove', 'post'} for op in parsed_ops):
+        raise Exception('Every op must be either create, add, remove, or post')
     if any('added_key' not in op for op in parsed_ops if op['type'] == 'add'):
         raise Exception('Every add operation must have an added_key')
     if any('removed_key' not in op for op in parsed_ops if op['type'] == 'remove'):
         raise Exception('Every remove operation must have a removed_key')
+    # added for exercise 2:
+    if any('message' not in op for op in parsed_ops if op['type'] == 'post'):
+        raise Exception('Every post operation must have a message')
+
+
 
     # Hash graph integrity: every op except the initial creation must reference at least one
     # predecessor operation, and all predecessors must exist in the set
@@ -76,31 +87,37 @@ def interpret_ops(ops):
         raise Exception('There must be exactly one create operation')
 
     # current group members are computed by the algorithm from the paper
-    return compute_membership(ops)
+    return compute_membership_and_messages(ops)
 
-def compute_membership(ops): 
+def compute_membership_and_messages(ops): 
     ops_by_hash = {hex_hash(op): verify_msg(op) for op in ops}
     seniority = compute_seniority(ops_by_hash) # of the form {pk : depth(add_op_of_pk), hash(add_op_of_pk)}
     auth_graph = authority_graph(ops_by_hash) # of the form {(add/create_or_rem_op_of_pk, (member, pk)) or (op1, op2)}
     # member_nodes filters out the (op1, op2) - only the ones of the form (add/create_or_rem_op_of_pk, (member, pk)) 
     member_nodes = {member_pk for _, member_pk in auth_graph if isinstance(member_pk, tuple) and member_pk[0] == 'member'}
+    # for exercise two:
+    message_nodes = { post_hash: post_info for post_hash, post_info  in ops_by_hash.items()  if post_info['type'] == 'post' } 
     cycles = uniquify([cycle for node in member_nodes for cycle in find_cycles(auth_graph, node, [])]) # of the form [[ op1_hash, op2_hash, ... , op1_hash, ...] .. ] ; uniquify because python does not allow sets in sets, have to work with frozen sets etc.
-    # By default max will compare the items by the first index. If the first index is the same then it'll compare the second index - https://stackoverflow.com/questions/18296755/python-max-function-using-key-and-lambda-expression
+    # By default max will compare the items by the first index. 
+    # If the first index is the same then it'll compare the second index - https://stackoverflow.com/questions/18296755/python-max-function-using-key-and-lambda-expression
     # and op as third argument just as a trick to still keep the op
     # the (member, pk) vertices do not have outgoing edges - by definition
     drop = { max([(seniority[subject(ops_by_hash[op_hash])], op_hash) for op_hash in cycle])
                 for cycle in cycles } # of the form (hash(add_op_of_pk_signing), hash(op))
     # the n1,n2 where neither the n1 nor the n2 are in drop (but drop is a 2-tuple, so we need to filter and see it is empty - not any(x) -> there is no x, x is empty)
     auth_graph2 = { (n1, n2) for n1, n2 in auth_graph if (not any(filter(lambda tuple: tuple[1] == n1, drop))) and (not any(filter(lambda tuple: tuple[1] == n2, drop)))}
-    # compute_validity for each (member, pk)
+
+    # compute_validity starting from the nodes without outgoing edges (member, pk) and the message nodes
     valid = {}
-    for node in member_nodes:
+    for node in [*member_nodes, *message_nodes]:
         valid = compute_validity(ops_by_hash, auth_graph2, node, valid)
-    return { pk for _, pk in member_nodes if valid['member', pk]}
+ 
+    return { 'members' : { pk for _, pk in member_nodes if valid['member', pk] },
+             'valid_messages' : { post_info['message'] for post_hash, post_info in message_nodes.items() if valid[post_hash]}}
 
 def subject(op):
     # you return the public key from the device that *does* the action
-    if op['type'] in ['create', 'add', 'remove']:
+    if op['type'] in ['create', 'add', 'remove', 'post']:
         return op['signed_by']
 
 def compute_seniority(ops_by_hash) :
@@ -133,8 +150,8 @@ def check_graph(ops_by_hash, op_hash, added, depth):
     # depth and added are empty, operation type is create
     elif not depth and not added and op['type'] == 'create' :
         return { (op['signed_by'] , op_hash)}, {op_hash : 0} # op['signed_by'] is the public key of the device 
-    # operation type is add or remove and it has predecessors
-    elif op['type'] in {'add', 'remove'} and op['preds']:
+    # operation type is add/remove/post and it has predecessors
+    elif op['type'] in {'add', 'remove', 'post'} and op['preds']:
         maxDepth = 0
         for predecessor_hash in op['preds']:
             added, depth = check_graph(ops_by_hash, predecessor_hash, added, depth)
@@ -148,7 +165,7 @@ def check_graph(ops_by_hash, op_hash, added, depth):
         depth.update({op_hash : maxDepth + 1})
         return (added, depth)
     else:
-        Exception("invalid graph - check_graph")
+        Exception("Invalid graph - check_graph")
 
 
 def authority_graph(ops_by_hash):
@@ -157,9 +174,11 @@ def authority_graph(ops_by_hash):
     OUTPUT the set of edges of the authority graph
     -> edge from op1 to op2 if op1 may affect whether op2 is authorised
     this means adding additional edges 
-    1) op1 adds the device with pk, op2 is acausally succeeding operation performed by device pk
+    1) op1 adds the device with pk, op2 is a causally succeeding operation performed by device pk
     2) op1 removes the device with pk, op2 is performed by that device, AND op1 precedes op2 or they are concurrent
     REMARK: again, need to use the hash of the operation instead of the operation itself because python does not like dictionaries
+
+    for exercise 2 only added that you do not need to add a member node if it is a post message
     """
     # idea: go over all ops, for each op
     ## check whether it is an add/create op -> (op1, (member, pk))
@@ -167,24 +186,25 @@ def authority_graph(ops_by_hash):
     ### (not) precedes op1, depending on op1
     authority_graph = set()
     for hash1, op1 in ops_by_hash.items():
-        # get the correct p_k
-        p_k = None
         op1_type = op1['type']
-        if op1_type == 'create':
-            p_k = op1['signed_by']; # for a created op, you want the creator
-        elif op1_type == 'add':
-            p_k = op1['added_key'] # for an add op, you want the p_k that is added
-        elif op1_type == 'remove':
-            p_k = op1['removed_key'] # for a remove op, you want the p_k of the removed device
+        # if the operation is post, no additional edges must be added since it does not affect the authorisation of any public keys
+        if op1_type != 'post':
+            # get the correct p_k
+            p_k = None
+            if op1_type == 'create':
+                p_k = op1['signed_by']; # for a created op, you want the creator
+            elif op1_type == 'add':
+                p_k = op1['added_key'] # for an add op, you want the p_k that is added
+            elif op1_type == 'remove':
+                p_k = op1['removed_key'] # for a remove op, you want the p_k of the removed device
 
-        # add an edge between every operation that may affect the permissions associated with the device that has p_k
-        authority_graph.add((hash1, ('member', p_k)))  
-
-        # check for dependencies and add those as extra edges to the graph
-        for hash2, op2 in ops_by_hash.items():
-            if op2['signed_by'] == p_k: # the operations done by the device with p_k
-                if ((op1_type == 'create' or op1_type == 'add') and precedes(ops_by_hash, hash1, op2)) or (op1_type == 'remove' and not precedes(ops_by_hash, hash2, op1)) :
-                    authority_graph.add((hash1, hash2))
+            # add an edge between every operation that may affect the permissions associated with the device that has p_k
+            authority_graph.add((hash1, ('member', p_k)))  
+            # check for dependencies and add those as extra edges to the graph
+            for hash2, op2 in ops_by_hash.items():
+                if op2['signed_by'] == p_k: # the operations done by the device with p_k
+                    if ((op1_type == 'create' or op1_type == 'add') and precedes(ops_by_hash, hash1, op2)) or (op1_type == 'remove' and not precedes(ops_by_hash, hash2, op1)) :
+                        authority_graph.add((hash1, hash2))
     return authority_graph
     
 def uniquify(lst_of_lsts):
@@ -227,7 +247,7 @@ def compute_validity(ops_by_hash, authority_graph, node, valid):
         def filter_function(rem_op_hash):
             rem_op = ops_by_hash[rem_op_hash]
             return rem_op['type'] == 'remove' and precedes(ops_by_hash, add_op_hash, rem_op)
-            # filter the remove operations that succeed the addop, if none (not any) -> you found it!
+            # filter the remove operations that succeed the op, if none (not any) -> you found it!
         if (add_op['type'] in ['add', 'create']) and (not any(filter(filter_function, op_hashes_incoming_edge_in_current_op))) :
             tmp_bool = True
             break
@@ -246,76 +266,121 @@ class TestAccessControlList(unittest.TestCase):
     friendly_name = {public_key: name for name, public_key in public.items()}
 
     def test_add_remove(self):
-        # Make some example ops
+        # Alice creates the group, adds Bob, and concurrently adds Carol, after both Alice removes Bob
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         add_c = add_op(self.private['alice'], self.public['carol'], [hex_hash(create)])
         rem_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(add_b), hex_hash(add_c)])
 
         # Compute group membership
-        members = interpret_ops({create, add_b, add_c, rem_b})
+        interpretation_results = interpret_ops({create, add_b, add_c, rem_b})
+        members = interpretation_results['members']
+
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice', 'carol'})
 
     def test_paper_figure_2(self):
+        # Alice creates the group, adds Bob and Carol one after the other, concurrently with the addition of Carol, Bob adds Dave
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         add_c = add_op(self.private['alice'], self.public['carol'], [hex_hash(add_b)])
         add_d = add_op(self.private['bob'], self.public['dave'], [hex_hash(add_b)])
 
-        members = interpret_ops({create, add_b, add_c, add_d})
+        # Compute group membership
+        interpretation_results = interpret_ops({create, add_b, add_c, add_d})
+        members = interpretation_results['members']
+
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice', 'bob', 'carol', 'dave'})
 
     def test_paper_figure_3(self):
+        # Alice creates the group, adds Bob, then removes Bob, and concurrently with the removal of Bob, bob adds Carol. 
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         remove_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(add_b)])
         add_c = add_op(self.private['bob'], self.public['carol'], [hex_hash(add_b)])
 
-        members = interpret_ops({create, add_b, remove_b, add_c})
+        # Compute group membership
+        interpretation_results = interpret_ops({create, add_b, remove_b, add_c})
+        members = interpretation_results['members']
+
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice'})
 
     def test_paper_figure_4(self):
+        # Alice creates the group, adds Bob, then Bob adds Carol, afterwards Alice removes Bob
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         add_c = add_op(self.private['bob'], self.public['carol'], [hex_hash(add_b)])
         remove_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(add_c)])
 
+        # Compute group membership
+        interpretation_results = interpret_ops({create, add_b, remove_b, add_c})
+        members = interpretation_results['members']
 
-        members = interpret_ops({create, add_b, remove_b, add_c})
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice', 'carol'})
 
     def test_paper_figure_6(self):
         # cycle in the authority graph!
+        # Alice creates the group, adds Bob, afterwards removes Bob. 
+        # Concurrently with the removal of Bob, Bob adds Carrol - and after the addition of Carol, she removes Alice
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         remove_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(add_b)])
         add_c = add_op(self.private['bob'], self.public['carol'], [hex_hash(add_b)])
         remove_a = remove_op(self.private['carol'], self.public['alice'], [hex_hash(add_c)])
 
-        members = interpret_ops({create, add_b, remove_b, add_c, remove_a})
+        # Compute group membership
+        interpretation_results = interpret_ops({create, add_b, remove_b, add_c, remove_a})
+        members = interpretation_results['members']
+
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice'})
 
     def test_mutual_removal(self):
+        # Alice creates the group, she adds Bob, then Bob and Alice concurrently remove each other.  
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         remove_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(add_b)])
         remove_a = remove_op(self.private['bob'], self.public['alice'], [hex_hash(add_b)])
 
-        members = interpret_ops({create, add_b, remove_b, remove_a})
+        # Compute group membership
+        interpretation_results = interpret_ops({create, add_b, remove_b, remove_a})
+        members = interpretation_results['members']
+
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice'})
 
     def test_mutual_removal_2(self):
         # to test the depth looks at the first add
-        ## B first removes A, then adds it again - then both concurrently remove each other, should be resolved according to greatest seniority, should still be a
+        # Alice creates the group, then adds Bob, Bob removes Alice and then adds Alice again. Afterwards, Bob and Alice concurrently remove each other.
+        # should be resolved according to greatest seniority, should still be Alice
         create = create_op(self.private['alice'])
         add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
         remove_a1 = remove_op(self.private['bob'], self.public['alice'], [hex_hash(add_b)])
         add_a = add_op(self.private['bob'], self.public['alice'], [hex_hash(remove_a1)])
         remove_a2 = remove_op(self.private['bob'], self.public['alice'], [hex_hash(add_a)])
         remove_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(add_a)])
+        
+        # Compute group membership  
+        interpretation_results = interpret_ops({create, add_b, remove_b, remove_a1, add_a, remove_a2})
+        members = interpretation_results['members']
 
-        members = interpret_ops({create, add_b, remove_b, remove_a1, add_a, remove_a2})
         self.assertEqual({self.friendly_name[member] for member in members}, {'alice'})
+
+
+
+    def test_post_then_remove(self):
+        """Test that a post made before removal remains valid"""
+      
+        # creating group, adding Bob. After Bob posts a valid message, Alice removes Bob 
+        create = create_op(self.private['alice'])
+        add_b = add_op(self.private['alice'], self.public['bob'], [hex_hash(create)])
+        post_b = post_op(self.private['bob'], "This is Bob", [hex_hash(add_b)])
+        rem_b = remove_op(self.private['alice'], self.public['bob'], [hex_hash(post_b)])
+
+        # Compute group membership  + valid posts
+        interpretation_results = interpret_ops({create, add_b, post_b, rem_b})
+        members = interpretation_results['members']
+        valid_messages = interpretation_results['valid_messages']
+
+        self.assertEqual({self.friendly_name[member] for member in members}, {'alice'})
+        self.assertEqual(valid_messages, {"This is Bob"})  # Bob's post should still be valid
  
 if __name__ == '__main__':
     unittest.main()
